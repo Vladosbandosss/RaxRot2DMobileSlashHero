f7a758da91f347b6604711fa3b4310c90b603275
Vladosbandosss <dasistperfektos@gmail.com>
2021-12-26 21:11:06 +0300

    change

---
+++ Assets/Scripts/GamePlay/DataManger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DataManger 
+{
+   public static void SaveData(string dataName, int value)
+   {
+      PlayerPrefs.SetInt(dataName,value);
+      
+   }
+
+   public static int GetDate(string dataName)
+   {
+      return PlayerPrefs.GetInt(dataName);
+   }
+}
---
+++ Assets/Scripts/GamePlay/GamePlayController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamePlayController : MonoBehaviour
+{
+    public static GamePlayController instance;
+
+    [HideInInspector] public int selectedCharacter = 0;
+
+    [SerializeField] private int char2UnlockSore = 10, char3UnlockScore = 20;
+
+    [SerializeField] private GameObject[] players;
+    
+    
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        PlayerPrefs.DeleteAll();///////
+        int gameData = DataManger.GetDate(TagManager.DATAINITIALIZED);
+      
+        if (gameData == 0)
+        {
+            //первый раз зпустил
+            selectedCharacter = 0;
+            DataManger.SaveData(TagManager.SELECTEDCHARACTERDATA,selectedCharacter);
+            DataManger.SaveData(TagManager.HIGHSCOREDATA,0);
+            
+            DataManger.SaveData(TagManager.CHARACTERDATA+ "0",1);
+            DataManger.SaveData(TagManager.CHARACTERDATA+ "1",0);
+            DataManger.SaveData(TagManager.CHARACTERDATA+ "2",0);
+            
+            DataManger.SaveData(TagManager.DATAINITIALIZED,1);
+        }else if (gameData == 1)
+        {
+            selectedCharacter = DataManger.GetDate(TagManager.SELECTEDCHARACTERDATA);
+        }
+    }
+
+    private void OnEnable()
+    {
+        //SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+       // SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    /*
+    private void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+    {
+        if (scene.name == TagManager.GAMEPLAYSCENENAME)
+        {
+            Instantiate(players[selectedCharacter]);
+            
+            Camera.main.GetComponent<CameraFollow>().FindPlayerRef();
+        }
+    }*/
+}
